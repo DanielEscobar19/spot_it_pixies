61ce925f287b61e1c44860ea0c68ab02a6716fb0
DanielEscobar19 <danielonchio@gmail.com>
2022-10-24 16:37:29 -0600

    Se agrega popper y jquery al index para usarlo en cualquier lado/

--- src/spot-it/src/index.js
+++ src/spot-it/src/index.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
