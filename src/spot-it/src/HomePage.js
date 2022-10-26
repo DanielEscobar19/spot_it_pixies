3ea3b7b6c379c3d1969344638adbb79d0b57234b
DanielEscobar19 <danielonchio@gmail.com>
2022-10-26 00:58:02 -0600

    Se avanza con logica de crear una sesion

--- src/spot-it/src/HomePage.js
+++ src/spot-it/src/HomePage.js
@@ -1,14 +1,47 @@
 import Layout from './Layout'
 import './css/Homepage/Homepage.css'
-import {useNavigate, Link} from 'react-router-dom';
+import './css/common/common.css'
+import { useNavigate } from 'react-router-dom';
+import React, { useRef } from 'react';
 
 export default function HomePage() {
-  // object used to navigate between views
-  const navigate = useNavigate();
+  const navigate  = useNavigate()
+  const userNameRef = useRef();
+  const SessionNameRef = useRef();
+  let sessionName = "";
+  let username = "";
+  function handleUsernameInput(e) {
+    username = userNameRef.current.value;
+    console.log(`Name: ${userNameRef.current.value}`);
+  }
+  
+  function handleSessionNameInput(e) {
+    sessionName = SessionNameRef.current.value;
+    console.log(`Session: ${SessionNameRef.current.value}`);
+  }
+  
+  
+  function createSession() {
+    if (sessionName === "") {
+      alert("You have to assign a name for the session.");
+    } else {
+      if (username === "") {
+        alert("Do not forget to create your gamertag!");
+      } else {
+  
+      }
+    
+      let sessionId = 1234;
+      navigate(`/new-session?session-id=${sessionId}`, {replace : true});
+    }
+  }
+  
 
-  const navigateHostWaitingRoom = () => {
-    navigate(`/WaitingRoomHost`);
-  };
+
+
+  function joinSession() {
+
+  }
 
   return (
     <section>
@@ -16,42 +49,42 @@ export default function HomePage() {
       <Layout>
         {/* <Layout /> */}
         {/* Welcome message and image */}
-        <section class="row d-flex flex-nowrap align-items-center justify-content-center mt-2">
-              <h1 class="Welcome-msg col-12 col-md-7 ">
+        <section className="row d-flex flex-nowrap align-items-center justify-content-center mt-2">
+              <h1 className="Welcome-msg col-12 col-md-7 ">
                 <b>Welcome to <u>The Well</u> <br/> 
                 A <u>Spot it</u> minigame</b>
               </h1>
-              <img src="../../img/Homepage/spot-it-hand.png" alt="Spot it hand decorative" class="col-4 dec-img-spot-it d-none d-md-block img-fluid"/>
+              <img src="../../img/Homepage/spot-it-hand.png" alt="Spot it hand decorative" className="col-4 dec-img-spot-it d-none d-md-block img-fluid"/>
         </section>
 
-        <section class="d-flex justify-content-center mt-3">
+        <section className="d-flex justify-content-center mt-3">
           <form action="Homepage.html" method="GET">
-            <div class="row inputName d-flex flex-column justify-content-center align-items-center">
-              <div class="col d-flex justify-content-center">
-                <div class="mb-4 text-center">
-                  <label for="playerName" class="form-label h2">Your name</label>
-                  <input type="text" class="form-control" id="playerName" placeholder="e.g: Timmy" autofocus size="22"/>
+            <div className="row inputName d-flex flex-column justify-content-center align-items-center">
+              <div className="col d-flex justify-content-center">
+                <div className="mb-4 text-center">
+                  <label className="form-label h2">Your name</label>
+                  <input ref={userNameRef} onInput={handleUsernameInput} type="text" className="form-control" id="playerName" placeholder="e.g: Timmy" autoFocus={true} size="22"/>
                 </div>
               </div>
             </div>
-            <div class="row sessions d-flex flex-wrap justify-content-center">
-              <div class="col col-6 mb-3">
-                <div class="card create-session">
-                  <h2 class="card-header">Create session</h2>
-                  <div class="card-body">
-                    <label for="sessionName" class="form-label h3">Session name</label>
-                    <input type="text" class="form-control mb-3" id="sessionName" placeholder="e.g: The golden game" size="50"/>
-                    <Link to="/WaitingRoomHost" class="btn btn-primary btn-lg">Create</Link>
+            <div className="row sessions d-flex flex-wrap justify-content-center">
+              <div className="col col-6 mb-3">
+                <div className="card create-session">
+                  <h2 className="card-header">Create session</h2>
+                  <div className="card-body">
+                    <label className="form-label h3">Session name</label>
+                    <input ref={SessionNameRef} onInput={handleSessionNameInput} type="text" className="form-control mb-3" id="sessionName" placeholder="e.g: The golden game" size="50"/>
+                    <button onClick={createSession} className="btn btn-primary btn-lg">Create</button>
                   </div>
                 </div>
               </div>
-              <div class="col col-6">
-                <div class="card join-session">
-                  <h2 class="card-header">Join session</h2>
-                  <div class="card-body">
-                    <label for="sessionPin" class="form-label h3">Session pin</label>
-                    <input type="text" class="form-control mb-3" id="sessionPin" placeholder="e.g: 1254" size="50"/>
-                    <a href="../WaitingRooms/guestWaitingRoom.html" class="btn btn-primary btn-lg">Join</a>
+              <div className="col col-6">
+                <div className="card join-session">
+                  <h2 className="card-header">Join session</h2>
+                  <div className="card-body">
+                    <label  className="form-label h3">Session pin</label>
+                    <input type="text" className="form-control mb-3" id="sessionPin" placeholder="e.g: 1254" size="50"/>
+                    <button className="btn btn-primary btn-lg">Join</button>
                   </div>
                 </div>
               </div>
