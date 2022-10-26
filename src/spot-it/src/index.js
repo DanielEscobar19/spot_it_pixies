1f455fd6d21d10028f9160bccf7b3b1ca712389f
DanielEscobar19 <danielonchio@gmail.com>
2022-10-26 17:03:13 -0600

    se crea una numero de sesion basew

--- src/spot-it/src/index.js
+++ src/spot-it/src/index.js
@@ -9,12 +9,21 @@ import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 import WaitingRoomHost from './WaitingRoomHost';
 import HomePage from './HomePage';
 
+// the first session will have the code 100
+let sessionPin = 100;
+
+const setSessionPin = (newValue) => {
+  sessionPin = newValue;
+  console.log(`base pin of app index: ${sessionPin}`);
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/home-page" element={<HomePage />} />
+        <Route path="/" element={<HomePage session={{sessionPin, setSessionPin}} />} />
+        <Route path="/home-page" element={<HomePage session={{sessionPin, setSessionPin}}/>} />
         <Route path="/new-session" element={<WaitingRoomHost />} />
     </Routes>
   </Router>
