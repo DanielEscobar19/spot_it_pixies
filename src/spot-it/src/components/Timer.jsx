299e2de9bbff2673dafc86151e28fdcf15f1803d
manuCR <manuelarroyoportilla@gmail.com>
2022-10-28 00:34:36 -0600

    validacion para no correr mas de un setInterval

--- src/spot-it/src/components/Timer.jsx
+++ src/spot-it/src/components/Timer.jsx
@@ -5,8 +5,11 @@ const Timer = () => {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [running, setRunning] = useState(false)
 
   useEffect(() => {
+    if (running) return;
+    setRunning(true)
     let time = 0;
     setInterval(increment, 1000);
 
@@ -17,7 +20,7 @@ const Timer = () => {
       setSeconds(Math.floor(time  % 60));
     }
     
-  }, []);
+  }, [running]);
 
   return (
     <div className="timer" role="timer">
