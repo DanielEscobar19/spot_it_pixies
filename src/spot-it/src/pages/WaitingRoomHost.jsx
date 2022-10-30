6de034b2234ac36f9d202828d6e224225748a98d
DanielEscobar19 <danielonchio@gmail.com>
2022-10-30 00:10:49 -0600

    se llama comonente que muestra los jugadores con colores diferentes

--- src/spot-it/src/pages/WaitingRoomHost.jsx
+++ src/spot-it/src/pages/WaitingRoomHost.jsx
@@ -1,10 +1,11 @@
 import '../css/waitingRooms/waitingRoom.scss'
 
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useSearchParams} from 'react-router-dom';
 
 import Layout from './Layout'
 import Button from '../components/Button';
+import ConnectedPlayers from '../components/ConnectedPlayers';
 
 export default function WaitingRoomHost() {
   const [searchParams, ] = useSearchParams();
@@ -15,6 +16,7 @@ export default function WaitingRoomHost() {
   console.log( "\n\nsession-pin received " + searchParams.get("session-pin"));
   console.log( "session-name received " + searchParams.get("session-name"));
   console.log( "host-name received " + searchParams.get("host-name"));
+  const [playersList,setPlayerList] = useState([{type: "host", name : searchParams.get("host-name"), isConnected : true}]);
 
 
   return (
@@ -62,66 +64,14 @@ export default function WaitingRoomHost() {
 
     </section>
 
-    {/* <!-- section with player names and status (waiting for players or ready) --> */}
-    <section className="container">
-
-      {/* <!-- Row with connected player names --> */}
-      <div className="col d-flex text-center flex-row flex-wrap justify-content-center mb-5">
-        
-        {/* <!-- name of the host player --> */}
-        <div  className="d-flex col-3 align-items-center justify-content-center" id="player-hostname-crown">
-          <span className="p-0 badge bg-transparent">
-            <img id="host-crown-icon" src="../../img/common/magic-crown.svg" alt="Crown icon"/>
-          </span>
-          <p className="mx-3 my-2 selectable-text-all" id="player-hostname">{searchParams.get("host-name")}</p>
-        </div>
-
-        {/* <!-- TODO: connecion dot chages to class player-connected if it is connected, else it uses class player-disconnected --> */}
-        {/* <!-- player with connection status dot --> */}
-        <div className="d-flex col-3 align-items-center justify-content-center">
-          <span className="bi bi-dot player-connected player-connection-dot unselectable-text "></span> 
-          <p className=" ms-2 me-3 my-2 p-0 green-color selectable-text-all">playername</p>
-        </div>
-        {/* <!-- player with connection status dot --> */}
-        <div className="d-flex col-3 align-items-center justify-content-center">
-          <span className="bi bi-dot player-connected player-connection-dot unselectable-text "></span> 
-          <p className=" ms-2 me-3 my-2 p-0 yellow-color selectable-text-all">playername</p>
-        </div>
-        {/* <!-- player with connection status dot --> */}
-        <div className="d-flex col-3 align-items-center justify-content-center">
-          <span className="bi bi-dot player-disconnected player-connection-dot unselectable-text "></span> 
-          <p className=" ms-2 me-3 my-2 p-0 red-color cyan-color selectable-text-all">playername</p>
-        </div>
-        {/* <!-- player with connection status dot --> */}
-        <div className="d-flex col-3 align-items-center justify-content-center">
-          <span className="bi bi-dot player-disconnected player-connection-dot unselectable-text "></span> 
-          <p className=" ms-2 me-3 my-2 p-0 dark-green-color selectable-text-all">playername</p>
-        </div>
-        {/* <!-- player with connection status dot --> */}
-        <div className="d-flex col-3 align-items-center justify-content-center">
-          <span className="bi bi-dot player-connected player-connection-dot unselectable-text "></span> 
-          <p className=" ms-2 me-3 my-2 p-0 pink-color selectable-text-all">playername</p>
-        </div>
-        {/* <!-- player with connection status dot --> */}
-        <div className="d-flex col-3 align-items-center justify-content-center">
-          <span className="bi bi-dot player-disconnected player-connection-dot unselectable-text "></span> 
-          <p className=" ms-2 me-3 my-2 p-0 orange-color selectable-text-all">playername</p>
-        </div>
-        {/* <!-- player with connection status dot --> */}
-        <div className="d-flex col-3 align-items-center justify-content-center">
-          <span className="bi bi-dot player-disconnected player-connection-dot unselectable-text "></span> 
-          <p className=" ms-2 me-3 my-2 p-0 gray-color selectable-text-all">playername</p>
-        </div>
-      </div>
-
-      {/* <!-- box indicating if we are still waiting for players --> */}
-      {/* <!-- TODO: This text only appears if there is no player connected apart from the host --> */}
-      <div className="col d-flex text-center justify-content-center">
-        <section className="box-container">
-          <h2 className="unselectable-text">Waiting for players...</h2>
-        </section>
-      </div>
-    </section>
+    <ConnectedPlayers playerList={playersList} setPlayerList={setPlayerList}/>
+    {/* <!-- box indicating if we are still waiting for players --> */}
+    {/* <!-- TODO: This text only appears if there is no player connected apart from the host --> */}
+    <div className="col d-flex text-center justify-content-center">
+      <section className="box-container">
+        <h2 className="unselectable-text">Waiting for players...</h2>
+      </section>
+    </div>
   </>
   )
 }
