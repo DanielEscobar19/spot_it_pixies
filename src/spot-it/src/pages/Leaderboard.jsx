b81bda5f7e53b0b51c579bb94fbda2c72ed8a35b
DanielEscobar19 <danielonchio@gmail.com>
2022-10-30 22:24:42 -0600

    Se peude viajar del leaderboard al menu y al waiting room

--- src/spot-it/src/pages/Leaderboard.jsx
+++ src/spot-it/src/pages/Leaderboard.jsx
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import PlayerLeadearBoard from '../components/PlayerLeadearBoard';
-
+import Button from '../components/Button';
 import Layout from './Layout'
-
+import "../css/pages/Leaderboard.css"
 
 export default function Leaderboard() {
   const location = useLocation();
@@ -26,7 +26,9 @@ export default function Leaderboard() {
         
         <div className="row align-items-end my-5">
           <div className="col d-flex justify-content-center">
-            <a type="button" className="btn btn-primary purple-background" href="../Homepage/Homepage.html">Back home</a>
+            <Link  to={"/home-page"} replace={true}> 
+              <Button title="Back home"/>
+            </Link>
           </div>
       
           <div className="col d-flex flex-column align-items-center">
@@ -37,7 +39,9 @@ export default function Leaderboard() {
           </div>
           
           <div className="col justify-content-start">
-              <a type="button" className="btn btn-success" href="../WaitingRooms/guestWaitingRoom.html">Play again</a>
+            <Link to={`/new-session?session-pin=${location.state.sessionPin}&host-name=${players.find(x => x.type === "host").name}&session-name=${location.state.sessionName}`} replace={true}> 
+              <Button title="Play again"/>
+            </Link>
           </div>
         
         </div>
@@ -51,27 +55,30 @@ export default function Leaderboard() {
             </div>
           </div>
         </div>
-        <div class="container-sm leaderboard-container mt-3">
-          <div class="row d-flex align-items-center mb-3 py-1">
-            <div class="col-4 ps-5 col-position">
+        <div className="container-sm leaderboard-container">
+          {/* <!--TODO reuse code for rows--> */}
+          {/* <!--Player 1--> */}
+          <div className="row d-flex align-items-center mb-3 py-1">
+            <div className="col-4 ps-5 col-position">
               <h2>Rank</h2>
             </div>
-            <div class="col-4 ps-5 col-name text-center">
-              <h2>Player name</h2>
+            <div className="col-4 ps-5 col-name text-center">
+              <h2>Name</h2>
+              <h3>personal best time</h3>
             </div>
-            <div class="col-4 pe-5 col-wincount text-center">
-              <h2>Victories</h2>
+            <div className="col-4 pe-5 col-wincount text-end">
+              <h2>Wincount</h2>
             </div>
           </div>
+          {
+            players.map((player, index) => {
+              if (index === 0) {
+                ++player.victories;
+              }
+              return <PlayerLeadearBoard key={player.id} player={player} rank={index + 1}/>
+            })
+          }
         </div>
-        {
-          players.map((player, index) => {
-            if (index === 0) {
-              ++player.victories;
-            }
-            return <PlayerLeadearBoard player={player}/>
-          })
-        }
     </>
   );
 }
