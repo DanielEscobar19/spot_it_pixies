04455861dc8a4107da80d2c8d462117a38a4361f
DanielEscobar19 <danielonchio@gmail.com>
2022-11-26 19:59:47 -0600

    Typo en nombre de carpeta: Img => img

--- src/spot-it/src/pages/Layout.jsx
+++ src/spot-it/src/pages/Layout.jsx
@@ -38,7 +38,7 @@ export default function Layout() {
     <header>
       <nav className="navbar w-100 navbar-expand-lg bg-light unselectable-text">
         <Link className="navbar-brand homeLogo unselectable-text" to="/home-page" onClick={onClick} replace="true">
-          <img src="../../Img/common/spot-it-logo.svg" alt="Spot it logo" className="img-fluid ms-4 me-0"/>
+          <img src="../../img/common/spot-it-logo.svg" alt="Spot it logo" className="img-fluid ms-4 me-0"/>
         </Link>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
